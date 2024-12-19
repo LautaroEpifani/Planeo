f92d670e82ef81d3db138c33f410772e92e4e9a2
Lautaro Epifani <epifanilautaro@gmail.com>
2024-12-19 09:59:04 +0100

    adding private route Board 4

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 
@@ -8,11 +8,17 @@ interface PrivateRouteProps {
 
 const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
   const { userLogged } = useAuth();
+  const [loading, setLoading] = useState(true);
 
-  if (userLogged === null) {
-    <Navigate to="/"/>;
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 500);
+    return () => clearTimeout(timer);
+  }, [userLogged]);
+  if (loading) {
+    <Navigate to="/" />;
   }
-
   return userLogged ? children : <Navigate to="/" />;
 };
 
